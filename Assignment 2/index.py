443df380730e04e557b5f252b394ac96abf2f1ea
Ryan Laverick <71405242+ryanlaverick@users.noreply.github.com>
2026-03-16 12:28:03 +0000

    rework application

--- Assignment 2/index.py
+++ Assignment 2/index.py
@@ -1,226 +1,323 @@
+from collections import OrderedDict
+from copy import copy, deepcopy
+from random import shuffle
 from time import sleep
-from Playlist import Playlist
-from Song import Song
 
 control_username = 'user123'
 control_password = 'Givemetheykey123'
-is_authenticated = False
 playlists = {}
 
-# Displays formatted messages to the user
-def print_formatted(message: list|str):
-    print('\n' * 20)
 
+class Song:
+    name = ''
+    artist = ''
+    genre = ''
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+    def get_name(self):
+        return self.name
+
+    def get_artist(self):
+        return self.artist
+
+    def get_genre(self):
+        return self.genre
+
+
+class Playlist:
+    name = ''
+    songs = {}
+
+    def __init__(self, name, songs=None):
+        self.name = name
+        self.songs = songs or {}
+
+    def get_name(self):
+        return self.name
+
+    def get_songs(self):
+        return self.songs
+
+    def set_name(self, name):
+        self.name = name
+
+    def add_song(self, name: str, artist: str, genre: str) -> bool:
+        if name in self.songs:
+            return False
+
+        self.songs[name] = Song(name, artist, genre)
+        return True
+
+    def remove_song(self, name: str) -> bool:
+        if name not in self.songs:
+            return False
+
+        del self.songs[name]
+        return True
+
+    def sort(self) -> Playlist:
+        songs_to_sort = deepcopy(self.songs)
+        songs_to_sort = sorted(songs_to_sort.items(), key=lambda item: item[0])
+
+        return Playlist(self.name, dict(songs_to_sort))
+
+    def shuffle(self) -> Playlist:
+        songs_to_shuffle = deepcopy(self.songs)
+        songs_to_shuffle = list(songs_to_shuffle.items())
+        shuffle(songs_to_shuffle)
+
+        return Playlist(self.name, dict(songs_to_shuffle))
+
+
+def pretty_print(message: list | str) -> None:
+    print(' ')
     print('**************************************************')
 
     if isinstance(message, str):
         print(message)
 
     if isinstance(message, list):
-        # Utilises the "spread" operator, which streams all items within a tuple/list/dictionary to display list-items nicely to the user
+        # Utilises Python's "spread" operator, which streams all items within a tuple/list/dictionary to display list-items nicely to the user
         print(*message, sep='\n')
 
     print('**************************************************')
 
-def display_actions():
-    print_formatted([
-        'Select an action to perform:',
-        '    1. Add a Playlist',
-        '    2. Rename a Playlist',
-        '    3. Remove a Playlist',
-        '    4. Add a Song to a Playlist',
-        '    5. Remove a Song from a Playlist',
-        '    6. Sort Playlist',
-        '    7. Shuffle Songs in a Playlist',
-        '    8. Exit Program',
-    ])
 
-def attempt_login():
-    login_attempts = 0
-    global is_authenticated
+def format_playlist(playlist: Playlist) -> list:
+    formatted_playlist = list()
 
-    while not is_authenticated:
-        if login_attempts == 3:
-            print_formatted('Could not verify account information! Suspending logins for 5 minutes...')
+    formatted_playlist.append(' ')
+    formatted_playlist.append(playlist.get_name())
 
-            sleep(300)
-            login_attempts = 0
+    for song in playlist.get_songs().values():
+        formatted_playlist.append(
+            '    • ' + song.get_name() + ' - ' + song.get_artist() + ' (' + song.get_genre() + ')')
 
-        username = str(input('Please enter your username: '))
-        password = str(input('Please enter your password: '))
+    return formatted_playlist
 
-        if username == control_username and password == control_password:
-            is_authenticated = True
-            break
 
-        login_attempts += 1
-        print_formatted('Please try again!')
+def display_playlists() -> None:
+    message = list(['Playlists:'])
 
-def add_playlist(name: str) -> bool:
-    if not name:
-        print_formatted('Unable to create Playlist as the name is blank!')
+    for playlist in playlists.values():
+        if type(playlist) is not Playlist:
+            continue
+
+        for item in format_playlist(playlist):
+            message.append(item)
+
+    pretty_print(message)
+
+
+def add_playlist(name: str, songs=None) -> bool:
+    if name in playlists:
         return False
 
-    target_playlist = playlists.get(name)
-
-    if isinstance(target_playlist, Playlist):
-        print_formatted(f'Unable to create Playlist with name {name} as it already exists!')
-        return False
-
-    playlists[name] = Playlist(name)
-
-    print_formatted([
-        'Playlists:',
-        map(lambda p: p.get_name(), *playlists)
-    ])
-
+    playlists[name] = Playlist(name, songs)
     return True
+
 
 def rename_playlist(old_name: str, new_name: str) -> bool:
-    if not old_name:
-        print_formatted('Unable to rename Playlist as the old name is blank!')
+    if old_name not in playlists:
         return False
 
-    if not new_name:
-        print_formatted('Unable to rename Playlist as the new name is blank!')
+    if new_name in playlists:
         return False
 
-    target_playlist = playlists.get(old_name)
+    playlist: Playlist = playlists[old_name]
+    playlists[new_name] = playlist
 
-    if not isinstance(target_playlist, Playlist):
-        print_formatted(f'Unable to rename Playlist {old_name} as it does not exist!')
-        return False
-
-    remove_playlist(old_name)
-    playlists[new_name] = target_playlist
+    del playlists[old_name]
 
     return True
 
-# This function will accept a string-name of a Playlist to remove, and will return:
-#    True - if the Playlist was removed successfully
-#    False - if the Playlist does not exist
+
 def remove_playlist(name: str) -> bool:
-    if not name:
-        print_formatted('Unable to remove Playlist as the name is blank!')
+    if name not in playlists:
         return False
 
-    target_playlist = playlists.get(name)
-
-    if not isinstance(target_playlist, Playlist):
-        print_formatted(f'Unable to remove Playlist {name} as it does not exist!')
-        return False
-
-    del target_playlist
+    del playlists[name]
     return True
 
-def add_song_to_playlist(playlist_name: str, song_name: str, song_artist: str, song_genre: str) -> bool:
-    if not playlist_name:
-        print_formatted('Unable to create Playlist as the name is blank!')
+
+def sort_playlist(name: str) -> list | bool:
+    if name not in playlists:
         return False
 
-    target_playlist = playlists.get(playlist_name)
+    playlist: Playlist = playlists[name]
+    return format_playlist(playlist.sort())
 
-    if not isinstance(target_playlist, Playlist):
-        print_formatted(f'Unable to add Song to Playlist {playlist_name} as it does not exist!')
+
+def shuffle_playlist(name: str) -> list | bool:
+    if name not in playlists:
         return False
 
-    if not song_name:
-        print_formatted('Unable to add Song to Playlist as the name is blank!')
-        return False
+    playlist: Playlist = playlists[name]
+    return format_playlist(playlist.shuffle())
 
-    if not song_artist:
-        print_formatted('Unable to add Song to Playlist as the artist is blank!')
-        return False
 
-    if not song_genre:
-        print_formatted('Unable to add Song to Playlist as the genre is blank!')
-        return False
+# Sets up default playlists
+add_playlist('70s', {
+    'Rocketman': Song('Rocketman', 'Elton John', 'Soft Rock'),
+    'Bohemian Rhapsody': Song('Bohemian Rhapsody', 'Queen', 'Hard Rock'),
+    'Dancing Queen': Song('Dancing Queen', 'ABBA', 'R&B'),
+    'Dreams': Song('Dreams', 'Fleetwood Mac', 'Indie'),
+    'Highway to Hell': Song('Highway to Hell', 'AC/DC', 'Rock'),
+})
 
-    target_playlist.add_song(song_name, song_artist, song_genre)
-    return True
+add_playlist('80s', {
+    'Another One Bites the Dust': Song('Another One Bites the Dust', 'Queen', 'Disco'),
+    'Back in Black': Song('Back in Black', 'AC/DC', 'Rock'),
+    'Everybody Wants to Rule the World': Song('Everybody Wants to Rule the World', 'Tears for Fears', 'Synth-pop'),
+    'Love Will Tear Us Apart': Song('Love Will Tear Us Apart', 'Joy Division', 'Post-Punk')
+})
 
-def remove_song_from_playlist(playlist_name, song_name) -> bool:
-    if not playlist_name:
-        print_formatted('Unable to create Playlist as the name is blank!')
-        return False
+add_playlist('2000s', {
+    'Mr. Brightside': Song('Mr. Brightside', 'The Killers', 'Rock'),
+    'Poker Face': Song('Poker Face', 'Lady Gaga', 'R&B'),
+    'The Scientist': Song('The Scientist', 'Coldplay', 'Rock'),
+    'Dance, Dance': Song('Dance, Dance', 'Fall Out Boy', 'Pop-Punk'),
+    'Umbrella': Song('Umbrella', 'Rihanna', 'R&B'),
+    'Lose Yourself': Song('Lost Yourself', 'Eminem', 'Hip-Hop'),
+    'Crazy in Love': Song('Crazy in Love', 'Beyoncé', 'R&B')
+})
 
-    target_playlist = playlists.get(playlist_name)
+pretty_print('Please log in to the system!')
 
-    if not isinstance(target_playlist, Playlist):
-        print_formatted(f'Unable to add Song to Playlist {playlist_name} as it does not exist!')
-        return False
+login_attempts = 0
+is_authenticated = False
 
-    # TODO Remove Song
+while not is_authenticated:
+    if login_attempts == 3:
+        pretty_print('Could not verify login information! Please try again in 5 minutes...')
 
-    return True
+        sleep(300)
+        login_attempts = 0
 
-def sort_playlist(playlist_name):
-    return
+    username = str(input('Please enter your username: '))
+    password = str(input('Please enter your password: '))
 
-def shuffle_playlist(playlist_name) -> bool:
-    if not playlist_name:
-        print_formatted('Unable to create Playlist as the name is blank!')
-        return False
+    if username == control_username and password == control_password:
+        is_authenticated = True
+        print('Logged in successfully!')
+        break
 
-    target_playlist = playlists.get(playlist_name)
-
-    if not isinstance(target_playlist, Playlist):
-        print_formatted(f'Unable to add Song to Playlist {playlist_name} as it does not exist!')
-        return False
-
-    target_playlist.shuffle_songs()
-    print_formatted(*target_playlist.get_songs())
-
-    return True
-
-def exit_program():
-    print_formatted("Goodbye!")
-    exit()
-
-print_formatted([
-    'Please select one of the following:',
-    '    1. Login',
-    '    2. Continue as Guest',
-])
-
-account_choice = input()
-while not type(account_choice) is int or int(account_choice) not in (1, 2):
-    print(f'Value {account_choice} is not recognised! Please try again...')
-    account_choice = input()
-
-if account_choice == 1:
-    attempt_login()
+    login_attempts += 1
+    pretty_print('Please try again!')
 
 continuing = True
-display_actions()
 while continuing:
-    choice = int(input('Please enter the numeric identifier of the action you would like to perform: '))
+    pretty_print([
+        'Select an action to perform:',
+        '    - [0] Display Playlists',
+        '    - [1] Add a Playlist',
+        '    - [2] Rename a Playlist',
+        '    - [3] Remove a Playlist',
+        '    - [4] Add a Song to a Playlist',
+        '    - [5] Remove a Song from a Playlist',
+        '    - [6] Sort Playlist',
+        '    - [7] Shuffle Songs in a Playlist',
+        '    - [8] Exit Program',
+    ])
 
-    if choice == 1:
-        while True:
-            playlist_name = str(input('Please enter a name for the new Playlist: '))
-            result = add_playlist(playlist_name)
+    # TODO Add non-int/non-accepted action handling
+    action = input('Enter the action you would like to perform (example 1): ')
 
-            if result:
-                break
+    while not action.isnumeric():
+        pretty_print(f'Unable to perform action "{action}". Please provide a number between 0 and 8!"')
+        action = input('Enter the action you would like to perform (example 1): ')
 
-    elif choice == 2:
-        while True:
-            old_playlist_name = str(input('Please enter the name of the Playlist you wish to rename: '))
-            new_playlist_name = str(input('Please enter the new name of the Playlist: '))
+    action = int(action)
+    while action not in range(0, 8):
+        pretty_print(f'Unable to perform action "{action}". Please provide a number between 0 and 8!"')
+        action = input('Enter the action you would like to perform (example 1): ')
 
-            result = rename_playlist(old_playlist_name, new_playlist_name)
+    if action == 0:
+        display_playlists()
+        sleep(2)
 
-            if result:
-                break
+    if action == 1:
+        name = str(input('Please enter a name for the new Playlist: '))
 
-    elif choice == 3:
-        while True:
-            playlist_name = str(input('Please enter the name of the Playlist you wish to remove: '))
-            result = remove_playlist(playlist_name)
+        while not name:
+            pretty_print('Unable to add a new Playlist as the name is blank! Please try again...')
+            name = str(input('Please enter a name for the new Playlist: '))
 
-            if result:
-                break
+        result = add_playlist(name)
 
-    elif choice == 8:
-        exit_program()
+        while not result:
+            pretty_print('A Playlist already exists with this name! Please try again...')
+            name = str(input('Please enter a name for the new Playlist: '))
+            result = add_playlist(name)
+
+        pretty_print('Successfully created new Playlist!')
+        sleep(2)
+
+    if action == 2:
+        break
+
+    if action == 3:
+        name = str(input('Please enter the name of a Playlist to remove: '))
+
+        while not name:
+            pretty_print('Unable to remove this Playlist as the name is blank! Please try again...')
+            name = str(input('Please enter the name of a Playlist to remove: '))
+
+        result = remove_playlist(name)
+
+        while not result:
+            pretty_print(f'Unable to remove the Playlist "{name}" as it does not exist! Please try again...')
+            name = str(input('Please enter the name of a Playlist to remove: '))
+            result = remove_playlist(name)
+
+        pretty_print('Successfully removed Playlist!')
+        sleep(2)
+
+    if action == 4:
+        break
+
+    if action == 5:
+        break
+
+    if action == 6:
+        name = str(input('Please enter the name of a Playlist to sort: '))
+
+        while not name:
+            pretty_print('Unable to sort this Playlist as the name is blank! Please try again...')
+            name = str(input('Please enter the name of a Playlist to sort: '))
+
+        result = sort_playlist(name)
+
+        while not result:
+            pretty_print(f'Unable to sort the Playlist "{name}" as it does not exist! Please try again...')
+            name = str(input('Please enter the name of a Playlist to sort: '))
+            result = sort_playlist(name)
+
+        pretty_print(result)
+        sleep(2)
+
+    if action == 7:
+        name = str(input('Please enter the name of a Playlist to shuffle: '))
+
+        while not name:
+            pretty_print('Unable to shuffle this Playlist as the name is blank! Please try again...')
+            name = str(input('Please enter the name of a Playlist to shuffle: '))
+
+        result = shuffle_playlist(name)
+
+        while not result:
+            pretty_print(f'Unable to shuffle the Playlist "{name}" as it does not exist! Please try again...')
+            name = str(input('Please enter the name of a Playlist to shuffle: '))
+            result = shuffle_playlist(name)
+
+        pretty_print(result)
+        sleep(2)
+
+    if action == 8:
+        pretty_print('Goodbye!')
+
+        continuing = False
